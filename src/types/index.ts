fab9e134b5c4ca48aeb2e259cb2f4b86b93d5b1f
Mikhail Ivanov <shirokopuh@gmail.com>
2026-03-06 20:02:10 +0700

    feat(components): add interfaces IProductResponse, IOrder, IOrderResponse

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,39 +1,54 @@
-export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
+export type ApiPostMethods = "POST" | "PUT" | "DELETE";
 
 export interface IApi {
     get<T extends object>(uri: string): Promise<T>;
-    post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
+    post<T extends object>(
+        uri: string,
+        data: object,
+        method?: ApiPostMethods,
+    ): Promise<T>;
 }
 
-export type TPayment = 'Cash' | 'Online' | ''
+export type TPayment = "Cash" | "Online" | "";
 
 export interface IProduct {
-  id: string;
-  description: string;
-  image: string;
-  title: string;
-  category: string;
-  price: number | null;
+    id: string;
+    description: string;
+    image: string;
+    title: string;
+    category: string;
+    price: number | null;
 }
 
-
-
 export interface IBuyer {
-  payment: TPayment;
-  email: string;
-  phone: string;
-  address: string;
-   
+    payment: TPayment;
+    email: string;
+    phone: string;
+    address: string;
 }
 
 export type TValidationRules<T> = {
-  [key in keyof T]: {
-    validateFn: ()=>boolean ;
-    message: string;
-  }
-}
+    [key in keyof T]: {
+        validateFn: () => boolean;
+        message: string;
+    };
+};
 
 export type TValidationErrorMessages<T> = {
-  [key in keyof T]: string;
+    [key in keyof T]: string;
+};
+
+export interface IProductResponse {
+    total: number;
+    items: IProduct[];
 }
 
+export interface IOrder extends IBuyer {
+    total: number;
+    items: IProduct["id"][];
+}
+
+export interface IOrderResponse {
+    id: string;
+    total: number;
+}
