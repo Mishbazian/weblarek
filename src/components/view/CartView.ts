2009f811624889276cf271f883ebac413bd09a55
Mikhail Ivanov <shirokopuh@gmail.com>
2026-03-17 12:42:22 +0700

    feat(components): add CartView component

---
+++ src/components/view/CartView.ts
@@ -0,0 +1,42 @@
+import { cloneTemplate, ensureElement } from "../../utils/utils";
+import { Component } from "../base/Component";
+
+interface ICart {
+    products: HTMLElement[];
+    cartPrice: number;
+    isCanOrder: boolean;
+}
+
+export class CartView extends Component<ICart> {
+    private productsList: HTMLUListElement;
+    private orderButton: HTMLButtonElement;
+    private priceElement: HTMLElement;
+    constructor() {
+        const cardContainer = cloneTemplate("#basket");
+        super(cardContainer);
+        this.productsList = ensureElement<HTMLUListElement>(
+            ".basket__list",
+            this.container,
+        );
+        this.priceElement = ensureElement<HTMLElement>(
+            ".basket__price",
+            this.container,
+        );
+        this.orderButton = ensureElement<HTMLButtonElement>(
+            ".basket__button",
+            this.container,
+        );
+        //this.orderButton.disabled = true;
+    }
+    set products(items: HTMLElement[]) {
+        this.productsList.replaceChildren();
+        items.forEach((item)=>
+        this.productsList.append(item))
+    }
+    set cartPrice(value: number) {
+        this.priceElement.textContent = value.toString();
+    }
+    set isCanOrder(value: boolean) {
+        this.orderButton.disabled = !value;
+    }
+}
