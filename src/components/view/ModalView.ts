b56eeba39b2fc80029af220523d4d25dd29ed8fe
Mikhail Ivanov <shirokopuh@gmail.com>
2026-03-21 23:55:28 +0700

    feat(components): add event emmitions in ModalView

--- src/components/view/ModalView.ts
+++ src/components/view/ModalView.ts
@@ -1,35 +1,39 @@
+import { IModal } from "../../types";
 import { ensureElement } from "../../utils/utils";
 import { Component } from "../base/Component";
+import { IEvents } from "../base/Events";
 
-interface IModal {
-    content: HTMLElement;
-}
-
+/**
+ * Модальное окно. Расширяет Component элементом для внутреннего контента и кнопкой закрытия. Передает событие клика на кнопку закрытия в брокер событий.
+ */
 export class ModalView extends Component<IModal> {
     private closeButton: HTMLButtonElement;
     private contentContainer: HTMLElement;
-    constructor(container: HTMLElement) {
+    /**
+     * @constructor создает экземпляр Модального кна
+     * @param events - интерфейс брокера событий
+     * @param container - контейнер модального окна
+     */
+    constructor(
+        private events: IEvents,
+        container: HTMLElement,
+    ) {
         super(container as HTMLElement);
         this.closeButton = ensureElement<HTMLButtonElement>(
             ".modal__close",
             this.container,
         );
         this.contentContainer = ensureElement<HTMLElement>(".modal__content");
+        this.container.addEventListener("click", (e) => {
+            if (e.target === e.currentTarget || e.target === this.closeButton) {
+                this.events.emit("modal:close");
+            }
+        });
     }
-
+    // установить содержимое модального окна
     set content(item: HTMLElement) {
         this.contentContainer.replaceChildren(item);
     }
-    private handleClick(e: Event) {
-        if (e.target === e.currentTarget || e.target === this.closeButton)
-            this.close();
-    }
-    open() {
-        this.container.classList.add("modal_active");
-        this.container.addEventListener("click", (e) => this.handleClick(e));
-    }
-    close() {
-        this.container.classList.remove("modal_active");
-        this.container.removeEventListener("click", (e) => this.handleClick(e));
-    }
+    set open(value: boolean) {
+        this.container.classList.toggle("modal_active", value);}
 }
