ef28103611c763781777a5c496c4bf0828a1bed6
Mikhail Ivanov <shirokopuh@gmail.com>
2026-03-22 14:28:11 +0700

    fix(types): add loses  types for view components

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
+
 export type ApiPostMethods = "POST" | "PUT" | "DELETE";
 
 export interface IApi {
@@ -19,9 +20,11 @@ export interface IProduct {
     category: string;
     price: number | null;
 }
-
+/**
+ * Данные покупателя
+ */
 export interface IBuyer {
-    payment: TPayment;
+    payment: TPayment; // вид оплаты
     email: string;
     phone: string;
     address: string;
@@ -42,10 +45,12 @@ export interface IProductResponse {
     total: number;
     items: IProduct[];
 }
-
+/**
+ * 
+ */
 export interface IOrder extends IBuyer {
-    total: number;
-    items: IProduct["id"][];
+    total: number; // - итоговая стоимость товаров в заказе
+    items: IProduct["id"][]; // - массив с id товаров в заказзе
 }
 
 export interface IOrderResponse {
@@ -56,6 +61,9 @@ export interface IOrderResponse {
 export interface IHeader {
     counter: number;
 }
+
+/**
+ * Базовый тип данных продукта для приведения к виду необходимому в карточках */ 
 export interface ICardProduct {
     id: string;
     title: string;
@@ -68,26 +76,76 @@ export interface ICardProduct {
     description: string;
 }
 
-export type TCardBase = Pick<ICardProduct, "title" | "price">
-export type TCardExt = Pick<ICardProduct, "image" | "category">
-
-export type TCardInfo = Pick<ICardProduct, "description">
+/**
+ * Тип - Объект с минимальным набором данных для карточки товара
+ */
+export type TCardBase = Pick<ICardProduct, "title" | "price">;
+/**
+ * Тип данных для установки в карточку товара изображения и категории
+ */
+export type TCardExt = Pick<ICardProduct, "image" | "category">;
+export type TCardInfo = Pick<ICardProduct, "description">;
+/**
+ * Тип данных расширяющий базовые данные картоки товара порядковым номером в корзине. Для карточки в корзине.
+ */
 export type TCardCart = TCardBase & {
     index: number;
-}
-export type TControlState = { text: string; disabled: boolean }
+};
+
+//Возможные значения состояния кнопки заказа в карточке
+export type TOrderButtonState = "add" | "remove" | "disabled";
+/**
+ * Тип данных необходимый для render в CardPreviw
+ */
 export type TCardPreview = TCardInfo & {
-    orderButtonState: TControlState;
+    state: TOrderButtonState;
+};
+//Тип данных для установки соответствия состояния кнопки и текста
+export type TCardStates = Record<TOrderButtonState, string>
+
+
+
+
+//Типы для событий
+/**
+ * Перечень алиасов для стандартных событий браузера
+ */
+export type TStandardEvents = "onClick" | "onChange" | "onSubmit";
+
+/**
+ * Тип функции обработчика стандартного события браузера
+ */
+export type TEventHandle = (event: Event) => void;
+
+/**
+ * Тип объекта с набором обработчиков стандартных событий с литералом в ключе
+ */
+export type TActions = Record<TStandardEvents, TEventHandle>;
+
+/**
+ * Тип объекта с обработчиком события "onClick"
+ */
+export type TCardActions = Pick<TActions, "onClick">;
+
+export type TFormActions = Pick<TActions, "onChange" | "onSubmit">
+/**
+ * Тип данных для рендера модального окна
+ */
+export interface IModal {
+    content: HTMLElement; //содержимое модального окна
+    open: boolean; // модальное окноЖ открыть да/нет
 }
 
+//Типы для форм
 export type TFormStatus = {
     isSubmitDisabled: boolean;
     error: string;
-}
-
+};
 
 export type TOrderPayment = "cash" | "card" | "";
 
 export type TFormPayment = {
     payment: TOrderPayment;
 };
+
+
