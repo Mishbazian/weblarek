bbe25fcc47906c2f78a444a795fe8ce63caec024
Mikhail Ivanov <shirokopuh@gmail.com>
2026-03-22 14:57:33 +0700

    feat(components): add events callbacks to OrderSuccessView

--- src/components/view/OrderSuccessView.ts
+++ src/components/view/OrderSuccessView.ts
@@ -1,26 +1,23 @@
+import { TCardActions, TOrderSuccess } from "../../types";
 import { ensureElement } from "../../utils/utils";
 import { Component } from "../base/Component";
 
-export type TOrderSuccess = {
-    total: string;
-};
+/**
+ * Окно сообщения об успешном оформлениии заказа. Расширяет Component элементом сообщения. Устанавливает слушатель события `click` на кнопку закрытия окна с обработчиком, полученным в конструкторе.
+ */
 export class OrderSuccessView extends Component<TOrderSuccess> {
     private totalElement: HTMLElement;
-    constructor(container: HTMLElement) {
+    constructor(container: HTMLElement, actions: TCardActions) {
         super(container);
         this.totalElement = ensureElement(
             ".order-success__description",
             this.container,
         );
-        this.container.addEventListener("click", (e) => {
-            if (
-                e.target instanceof HTMLButtonElement &&
-                e.target.classList.contains("order-success__close")
-            ) {
-                console.log("success close")
-                //todo
-            }
-        });
+        const closeButton = ensureElement(
+            ".order-success__close",
+            this.container,
+        );
+        closeButton.addEventListener("click", actions.onClick);
     }
     set total(value: string) {
         this.totalElement.textContent = `Списано ${value}`;
