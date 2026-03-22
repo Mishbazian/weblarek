f959c908072eed108d35a6cea6edea4e19c57098
Mikhail Ivanov <shirokopuh@gmail.com>
2026-03-22 15:56:59 +0700

    feat(components): update FormOrderView constructor to support changes
    to support toggler's and FormView changes

--- src/components/view/Form/FormOrderView.ts
+++ src/components/view/Form/FormOrderView.ts
@@ -1,22 +1,33 @@
-import { TFormPayment } from "../../../types";
+import { TFormActions, TFormPayment } from "../../../types";
 import { FormTogglerButtons } from "./FormTogglerButtons";
 import { FormView } from "./FormView";
 
-
+/**
+ * интерфейс для кнопочного переключателя формы
+ */
 export interface IFormToggler {
-    controls: HTMLButtonElement[];
-    input: HTMLInputElement;
-    set activeButton(value: string);
+    controls: HTMLButtonElement[]; // список кнопок переключателя
+    input: HTMLInputElement; // скрытый инпут
+    set activeButton(value: string); // установить активную кнопку
 }
 
+/**
+ * форма выбора способа оплаты и адреса доставки (наследует `FormView<TFormPayment>`). Использует `FormTogglerButtons` через композицию.
+ */
 export class FormOrderView extends FormView<TFormPayment> {
     private paymentToggler: IFormToggler;
-    constructor(container: HTMLElement) {
-        super(container);
+    /**
+     * @constructor создает экземпляяр формы
+     * @param {HTMLElement} container - ссылка на DOM элемент содержащий форму
+     * @param {object} actions - объект с коллбэк-функциями для обработки событий формы
+     */
+    constructor(container: HTMLElement, actions: TFormActions) {
+        super(container, actions);
         this.paymentToggler = new FormTogglerButtons(
             container,
-            ".button_alt",
-            "payment",
+            ".button_alt", // селектор кнопок переключателя
+            "payment", // имя данных в форме
+            "button_alt-active", // имя класса активной кнопки
         );
     }
 
