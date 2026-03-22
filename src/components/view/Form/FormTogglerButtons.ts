9af853454419e984a53b4adff6f403fdcd84fd6a
Mikhail Ivanov <shirokopuh@gmail.com>
2026-03-22 15:33:05 +0700

    feat(components): add activeButtonClassname to FormTogglersButton constructor

--- src/components/view/Form/FormTogglerButtons.ts
+++ src/components/view/Form/FormTogglerButtons.ts
@@ -5,14 +5,24 @@ import {
 } from "../../../utils/utils";
 import { IFormToggler } from "./FormOrderView";
 
-export class FormTogglerButtons implements IFormToggler{
+/**
+ * Вспомогательный класс для использования кнопок в качестве переключателя. * Создает скрытый инпут. в который по клику на кнопке передает в инпут значение `name` кнопки и генерирует событие `change` для централизованной обработки. Позволяет установить класс для отображения активной кнопки(самостоятельное переключение класса по клику исключено).
+ */
+export class FormTogglerButtons implements IFormToggler {
     controls: HTMLButtonElement[];
     input: HTMLInputElement;
-
+    /**
+     * @constructor создает экземпляр переключателя
+     * @param {HTMLElement} container - родительский контейнер для всех кнопок переключателя
+     * @param {string} selector - селектор для поиска кнопок (должен быть одинаковым для всех кнопок)
+     * @param {string} inputName  - имя скрытого инпута, устанавливается в `input.name` для корректного сбора FormDate.
+     * @param {string} activeButtonClassName - имя класса для активной кнопки
+     */
     constructor(
         private container: HTMLElement,
         selector: SelectorCollection<HTMLButtonElement>,
         inputName: string,
+        private readonly activeButtonClassName: string,
     ) {
         this.controls = ensureAllElements(selector, this.container);
         this.input = createElement<HTMLInputElement>("input", {
@@ -32,10 +42,13 @@ export class FormTogglerButtons implements IFormToggler{
         });
     }
 
-    set activeButton(value: string) {
+    set activeButton(name: string) {
         this.controls.forEach((button) =>
-            button.classList.toggle("button_alt-active", value === button.name),
+            button.classList.toggle(
+                this.activeButtonClassName,
+                name === button.name,
+            ),
         );
-        this.input.value = value;
+        this.input.value = name;
     }
 }
