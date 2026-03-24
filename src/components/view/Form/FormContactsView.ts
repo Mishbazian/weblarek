747c96159b7bae02fa040ff62416da5cfc2601b3
Mikhail Ivanov <shirokopuh@gmail.com>
2026-03-24 09:00:57 +0700

    fix(components): fix typo in classname FormContactsView

--- src/components/view/Form/FormContactsView.ts
+++ src/components/view/Form/FormContactsView.ts
@@ -1,7 +1,7 @@
 import { IEvents } from "../../base/Events";
 import { FormView } from "./FormView";
 
-export class FormContactView extends FormView<void> {
+export class FormContactsView extends FormView<void> {
     constructor(events: IEvents, container: HTMLElement ) {
         super(events, container);
     }
