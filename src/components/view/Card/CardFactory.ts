79047b58513f0694237b79f277a538ffee9f2eb2
Mikhail Ivanov <shirokopuh@gmail.com>
2026-03-26 03:19:11 +0700

    feat(components): add CardFactory

--- src/components/view/Card/CardFactory.ts
+++ src/components/view/Card/CardFactory.ts
@@ -1,20 +1,23 @@
-import { ICardFactory, IComponent, TCardActions, TCardCartConstructor, TCardCatalogueConstructor, TCardCatalogueView, TCardPreviewConstructor } from "../../../types";
+import { ICardFactory, IComponent, TCardActions, TCardCartConstructor, TCardCartView, TCardCatalogueConstructor, TCardCatalogueView, TCardPreviewConstructor, TCardPreviewView } from "../../../types";
+import { VIEW_SELECTORS } from "../../../utils/constants";
 import { cloneTemplate } from "../../../utils/utils";
-
+import { CardCartView } from "./CardCartView";
+import { CardPreviewView } from "./CardPreviewView";
+const settings = VIEW_SELECTORS.CARD
 export class CardFactory implements ICardFactory {
-    constructor(
-        private cardCart: TCardCartConstructor,
-        private cardPreview: TCardPreviewConstructor,
-        private cardCatalogue: TCardCatalogueConstructor,
+    constructor(        
+        private cardCart: TCardCartConstructor = CardCartView,
+        private cardPreview: TCardPreviewConstructor = CardPreviewView,
+        private cardCatalogue: TCardCatalogueConstructor = CardPreviewView,
     ) {}
 
-    createCardCart(actions: TCardActions) {
-        return new this.cardCart(cloneTemplate("#card-basket"), actions);
+    createCardCart(actions: TCardActions): IComponent<TCardCartView> {
+        return new this.cardCart(cloneTemplate(settings.CART), actions);
     }
-    createCardPreview(actions?: TCardActions) {
-        return new this.cardPreview(cloneTemplate("#card-preview"), actions);
+    createCardPreview(actions?: TCardActions): IComponent<TCardPreviewView> {
+        return new this.cardPreview(cloneTemplate(settings.PREVIEW), actions);
     }
     createCardCatalogue(actions: TCardActions): IComponent<TCardCatalogueView> {
-        return new this.cardCatalogue(cloneTemplate("#card-catalog"), actions);
+        return new this.cardCatalogue(cloneTemplate(settings.CATALOGUE), actions);
     }
 }
